db331054875dba05c23b4b73590416e1cf90c7f0
João Carlos Silva de Assis <joaocarlosdeassis@poli.ufrj.br>
2021-09-13 21:42:40 -0300

    Switch case

--- teste.dart
+++ teste.dart
@@ -1,3 +1,11 @@
 void main() {
-  print(20 > 19 ? 'maior' : 'menor');
+  int choice = 10;
+  switch(choice){
+    case 1: print("it's one");
+      break;
+    case 2: print("it's two");
+      break;
+    default: print("it's the $choice");
+  }
+
 }
