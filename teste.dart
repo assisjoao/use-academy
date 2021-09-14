6053ca78000be99991cdbe401dae5cb846c52f35
João Carlos Silva de Assis <joaocarlosdeassis@poli.ufrj.br>
2021-09-13 21:45:11 -0300

    For in List

--- teste.dart
+++ teste.dart
@@ -1,11 +1,7 @@
 void main() {
-  int choice = 10;
-  switch(choice){
-    case 1: print("it's one");
-      break;
-    case 2: print("it's two");
-      break;
-    default: print("it's the $choice");
+  List<int> valores = [1, 2, 3];
+  for(int value in valores){
+    print(value);
   }
 
 }
