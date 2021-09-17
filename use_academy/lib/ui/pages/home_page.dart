ec273f29eb53698d9c8a094f057963fb79a45143
João Carlos Silva de Assis <joaocarlosdeassis@poli.ufrj.br>
2021-09-17 19:47:47 -0300

    feat: using GestureDetector

--- use_academy/lib/ui/pages/home_page.dart
+++ use_academy/lib/ui/pages/home_page.dart
@@ -9,27 +9,13 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: SizedBox.expand(
-            child: ListView.separated(
-              itemCount: 20,
-              itemBuilder: (context, index) => ListTile(
-                title: const Text('Título'),
-                subtitle: const Text('Sub Título'),
-                leading: CircleAvatar(
-                  child: const Text('G'),
-                  backgroundImage: Image.asset('lib/assets/images/image01.jpg').image,
-                  foregroundColor: Colors.blue,
-                  ),
-                  // trailing: InkWell(
-                  //   onTap: () => debugPrint('onTap no trailing: $index'),
-                  //   child: const Icon(Icons.edit),
-                  // ),
-                  tileColor: Colors.blue[100],
-                  onTap: () => debugPrint('onTap no trailing: $index'),
-                  horizontalTitleGap: 32.0,
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.0)),
-              ), 
-              separatorBuilder: (context, index) => const SizedBox(height: 16.0),
+          child: Container(
+            color: Colors.amber,
+            child: GestureDetector(
+              onDoubleTapDown: (details) => debugPrint(details.localPosition.dx.toString()),              
+              child: Container(color: Colors.green),
             ),
+          ),
         ),
       ),
     );
