56d24d85a6e7c7a09b701371d50c969ef0ab7a11
João Carlos Silva de Assis <joaocarlosdeassis@poli.ufrj.br>
2021-09-17 19:36:26 -0300

    feat: ListTile

--- use_academy/lib/ui/pages/home_page.dart
+++ use_academy/lib/ui/pages/home_page.dart
@@ -9,30 +9,27 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: SizedBox.expand(
-          child: Container(
-            color: Colors.white,
-            child: Wrap(
-              spacing: 16.0,
-              alignment: WrapAlignment.center,
-              children: [
-                Chip(
-                  label: const Text('Teste Wrap'),
-                  deleteIcon: const Icon(Icons.close_rounded),
-                  onDeleted: () {},
-                ),
-                Chip(
-                  label: const Text('Teste Wrap'),
-                  deleteIcon: const Icon(Icons.close_rounded),
-                  onDeleted: () {},
-                ),
-                Chip(
-                  label: const Text('Teste Wrap'),
-                  deleteIcon: const Icon(Icons.close_rounded),
-                  onDeleted: () {},
-                ),
-              ],
+            child: ListView.separated(
+              itemCount: 20,
+              itemBuilder: (context, index) => ListTile(
+                title: const Text('Título'),
+                subtitle: const Text('Sub Título'),
+                leading: CircleAvatar(
+                  child: const Text('G'),
+                  backgroundImage: Image.asset('lib/assets/images/image01.jpg').image,
+                  foregroundColor: Colors.blue,
+                  ),
+                  // trailing: InkWell(
+                  //   onTap: () => debugPrint('onTap no trailing: $index'),
+                  //   child: const Icon(Icons.edit),
+                  // ),
+                  tileColor: Colors.blue[100],
+                  onTap: () => debugPrint('onTap no trailing: $index'),
+                  horizontalTitleGap: 32.0,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.0)),
+              ), 
+              separatorBuilder: (context, index) => const SizedBox(height: 16.0),
             ),
-          ),
         ),
       ),
     );
