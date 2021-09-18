bca02a6d29af46bf8255ef657c29d27da4893b52
João Carlos Silva de Assis <joaocarlosdeassis@poli.ufrj.br>
2021-09-17 21:46:59 -0300

    feat: StatelessWidget

--- use_academy/lib/ui/pages/home_page.dart
+++ use_academy/lib/ui/pages/home_page.dart
@@ -1,4 +1,3 @@
-import 'dart:math' as math;
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -9,16 +8,29 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: SizedBox.expand(
-          child: Container(
-            child: IgnorePointer(
-              child: ElevatedButton(
-                onPressed: () => debugPrint('apertou'),
-                child: const Text('Button'),
+          child: Column(
+            children: [
+              const FirstContainer(),
+              Expanded(
+                child:Container(color: Colors.green[100]),
               ),
-            ),
+            ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class FirstContainer extends StatelessWidget {
+  const FirstContainer({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child:Container(color: Colors.blue[100]),
     );
   }
 }
