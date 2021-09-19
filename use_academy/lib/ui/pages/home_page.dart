cfb987c64d39a25a9af050f3192b51878254f553
João Carlos Silva de Assis <joaocarlosdeassis@poli.ufrj.br>
2021-09-19 20:42:17 -0300

    feat: first page done

--- use_academy/lib/ui/pages/home_page.dart
+++ use_academy/lib/ui/pages/home_page.dart
@@ -6,31 +6,70 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: SizedBox.expand(
-          child: Column(
-            children: [
-              const FirstContainer(),
-              Expanded(
-                child:Container(color: Colors.green[100]),
+      appBar: AppBar(
+        title: const Text('Perguntas e Respostas'),
+        centerTitle: true,
+      ),
+      body: Center(
+          child: Container(
+            alignment: Alignment.center,
+            color: Colors.blue[200],
+            child: Container(
+              height: 200,
+              margin: const EdgeInsets.all(30),
+              padding: const EdgeInsets.all(30),
+              alignment: Alignment.topCenter,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(36),
+                color: Colors.white,
               ),
-            ],
-          ),
+              child: Column(
+                crossAxisAlignment : CrossAxisAlignment.start,
+	              // mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children:[
+                  const Center (
+                    child: Text(
+                      'Perguntas e Respostas',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue,
+                      ),
+                    ),
+                  ),
+                  const Center (
+                    child: Text(
+                    'Responda 10 perguntas e veja sua pontuação no fim!',
+                    style: TextStyle(
+                      fontSize: 11,
+                    ),
+                  ),
+                  ),
+                  SizedBox (
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16),
+                          ),
+                        ),
+                      ),
+                      onPressed: () {
+                      //startGame(),
+                      },
+                      child: const Text(
+                        'START',
+                      textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
         ),
       ),
-    );
-  }
-}
-
-class FirstContainer extends StatelessWidget {
-  const FirstContainer({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child:Container(color: Colors.blue[100]),
     );
   }
 }
